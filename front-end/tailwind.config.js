9d98b1273f03d70fbbf2313da2f08604ee441572
@tranvantruong27082001 <tranvantruong27082001@gmail.com>
2024-10-23 11:50:29 +0700

    base construction

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+     extend: {
+        colors: {
+          white:" #ffffff",
+          bg:"#F3F4F6",
+          bgMess:"#6366F1",
+          textMess:"text-gray-600"
+        },
+        screens: {
+          
+        },
+     },
+     fontFamily: {
+      publicSans:["Public Sans", sans-serif],
+     },
+  },
+  plugins: [],
+};
